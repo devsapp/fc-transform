37d7c8e007b31d717b584a2dc6f199d35bba6566
wss-git <wssgryx@163.com>
2021-07-23 16:39:55 +0800

    feat: support deploy multiple logstore

--- src/lib/transform/log.ts
+++ src/lib/transform/log.ts
@@ -6,26 +6,31 @@ const COMPONENT = 'devsapp/sls';
 export default class Transform extends Base {
   transform(name: string, resource: any) {
     const servicesObj = {};
+    console.log('resource ', resource);
     
+    const logstores = [];
     _.forIn(resource, (v: any, logstoreName) => {
       if ((v || {}).Type === 'Aliyun::Serverless::Log::Logstore') {
-        const props: any = {
-          regionId: this.VARS_REGION,
-          project: name,
-          description: resource?.Properties?.Description || '',
-          logstore: logstoreName,
-          logstoreOption: {
+        logstores.push({
+          name: logstoreName,
+          option: [{
             ttl: v.Properties.TTL,
             shardCount: v.Properties.ShardCount,
-          }
-        };
-
-        servicesObj[name] = {
-          component: COMPONENT,
-          props,
-        };
+          }]
+        })
       }
     });
+    const props: any = {
+      regionId: this.VARS_REGION,
+      project: name,
+      description: resource?.Properties?.Description || '',
+      logstore: logstores,
+    };
+
+    servicesObj[name] = {
+      component: COMPONENT,
+      props,
+    };
 
     return servicesObj;
   }
