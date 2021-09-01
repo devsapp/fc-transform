c78d3e26fed911710c8bb32c445fd2ec0124a63a
wss-git <wssgryx@163.com>
2021-09-01 09:29:01 +0800

    build

--- lib/common/entity.d.ts
+++ lib/common/entity.d.ts
@@ -2,14 +2,6 @@ export interface ICredentials {
     AccountID?: string;
     AccessKeyID?: string;
     AccessKeySecret?: string;
-    SecretID?: string;
-    SecretKey?: string;
-    SecretAccessKey?: string;
-    KeyVaultName?: string;
-    TenantID?: string;
-    ClientID?: string;
-    ClientSecret?: string;
-    PrivateKeyData?: string;
 }
 export interface InputProps {
     props: any;
